6801a92f666d87756c8948d3e8e2e1e68191c42d
Barry Hoggard <barry@artsymail.com>
2015-08-13 15:17:23 -0400

    small copy fixes, show 20 artists

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,5 +1,5 @@
 class MainController < ApplicationController
   def index
-    @artists = Artist.all.order('score DESC').limit(10)
+    @artists = Artist.all.order('score DESC').limit(20)
   end
 end
